af1c8b0e77899cbee11370f9efdbfdc61d27a243
Tome Pajkovski <tome.pajkovski@devvironment.com>
2024-05-22 09:42:24 +0200

    starting the native block

---
+++ plugins/wp-native-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '96b11f0d73e85832e8fe');
---
+++ plugins/wp-native-blocks/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd85ca7f867dea999910d');
