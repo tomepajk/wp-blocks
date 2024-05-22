e6c69a2fde85db4b9291aa53739bc340d1cd67ad
Tome Pajkovski <tome.pajkovski@devvironment.com>
2024-05-22 10:37:51 +0200

    adding back the inspector controls

--- plugins/wp-native-blocks/build/index.asset.php
+++ plugins/wp-native-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ce1767ea63624c24e52c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '97e6cace40280b70d86f');
---
+++ plugins/wp-native-blocks/build/render.php
@@ -0,0 +1,18 @@
+<div class="two-halves-block">
+	<div class="image_id-side">
+		<img fetchpriority="high" decoding="async" width="640" height="427" src="http://wp-blocks.local/wp-content/uploads/2024/05/dog-7719758_640.jpg" class="attachment-full size-full" alt="" srcset="http://wp-blocks.local/wp-content/uploads/2024/05/dog-7719758_640.jpg 640w, http://wp-blocks.local/wp-content/uploads/2024/05/dog-7719758_640-300x200.jpg 300w" sizes="(max-width: 640px) 100vw, 640px">			</div>
+	<div class="content-side">
+		<div class="eyebrow">
+			Eyebrow
+		</div>
+		<h2 class="title">
+			Title
+		</h2>
+		<p class="description">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<a class="button" href="https://www.google.com">
+			Click Here
+		</a>
+	</div>
+</div>
